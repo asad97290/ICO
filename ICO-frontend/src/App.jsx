90979d4895e50f05650fb1e1ab5850dcceb723f1
Asadullah Khan <aukhan@akru.co>
2023-12-17 22:49:08 +0500

    update smart contract logic

--- ICO-frontend/src/App.jsx
+++ ICO-frontend/src/App.jsx
@@ -1,9 +1,10 @@
 import { useState, useEffect } from "react";
 import Web3 from "web3";
-import "./App.css";
+import axios from "axios";
 import { address, abi } from "./contracts/ICO.json";
 import Token from "./contracts/Token.json";
-import axios from "axios"
+import "./App.css";
+
 function App() {
   const [accounts, setAccounts] = useState(null);
   const [contract, setContract] = useState(null);
@@ -14,6 +15,10 @@ function App() {
   const [tokenBalance, setTokenBalance] = useState(0);
   const [txData, setTxData] = useState([]);
   const [isAdmin, setIsAdmin] = useState(false);
+  const [startDate, setStartDate] = useState(0);
+  const [endDate, setEndDate] = useState(0);
+  const [softCap, setSoftCap] = useState(0);
+  const [hardCap, setHardCap] = useState(0);
 
   useEffect(() => {
     (async function () {
@@ -22,7 +27,7 @@ function App() {
   }, []);
 
   async function loadWeb3() {
-    if (window.ethereum) {
+    if (window?.ethereum) {
       try {
         let web3 = new Web3(window.ethereum);
         setWeb3(web3);
@@ -35,27 +40,56 @@ function App() {
           ICO.methods.softCap().call(),
           ICO.methods.startTime().call(),
           ICO.methods.closeTime().call(),
-          ICO.methods.hasRole(web3.utils.soliditySha3("OWNER_ROLE"),accounts[0]).call(),
+          ICO.methods
+            .hasRole(web3.utils.soliditySha3("OWNER_ROLE"), accounts[0])
+            .call(),
           ICO.methods.raisedAmount().call(),
-          ICO.methods.contributions(accounts[0]).call()
-        ])
-        setIsAdmin(res[3])
+          ICO.methods.contributions(accounts[0]).call(),
+          ICO.methods.hardCap().call(),
+        ]);
+        console.log(res);
+        setSoftCap(web3.utils.fromWei(res[0], "ether"));
+        setHardCap(web3.utils.fromWei(res[6], "ether"));
+        setStartDate(
+          new Intl.DateTimeFormat("en-US", {
+            year: "numeric",
+            month: "long",
+            day: "numeric",
+            hour: "numeric",
+            minute: "numeric",
+            hour12: true,
+          }).format(new Date(Number(res[1]) * 1000))
+        );
+
+        setEndDate(
+          new Intl.DateTimeFormat("en-US", {
+            year: "numeric",
+            month: "long",
+            day: "numeric",
+            hour: "numeric",
+            minute: "numeric",
+            hour12: true,
+          }).format(new Date(Number(res[2]) * 1000))
+        );
+        setIsAdmin(res[3]);
         let TokenContract = new web3.eth.Contract(Token.abi, Token.address);
         console.log(ICO);
         setContract(ICO);
-        let wei = await TokenContract.methods.balanceOf(accounts[0]).call()
-        console.log("wei",wei)
-        let bal = web3.utils.fromWei(wei,"ether")
-        setTokenBalance(bal)
-        let raisedAmount = res[4]
-        console.log(raisedAmount);
-        let lockBalance = res[5]
+        let wei = await TokenContract.methods.balanceOf(accounts[0]).call();
+        console.log("wei", wei);
+        let bal = web3.utils.fromWei(wei, "ether");
+        setTokenBalance(bal);
+        let raisedAmount = res[4];
+        console.log("raisedAmount ", raisedAmount);
+        let lockBalance = res[5];
         console.log(lockBalance.amount);
         setRaisedAmount(web3.utils.fromWei(raisedAmount, "ether"));
         setLockBalance(web3.utils.fromWei(lockBalance.amount, "ether"));
-        let {data} = await axios.get(`http://localhost:3333/api/v1/getTxDetails/${accounts[0]}`)
-        setTxData(data.data)
-        console.log(data.data)
+        let { data } = await axios.get(
+          `http://localhost:3333/api/v1/getTxDetails/${accounts[0]}`
+        );
+        setTxData(data.data);
+        console.log(data.data);
       } catch (error) {
         console.log(error);
       }
@@ -67,12 +101,10 @@ function App() {
       let gasPrice = await web3.eth.getGasPrice();
       console.log("gasPrice", gasPrice);
 
-      let gas = await contract.methods
-        .invest()
-        .estimateGas({
-          from: accounts,
-          value: web3.utils.toWei(value.toString(), "ether"),
-        });
+      let gas = await contract.methods.invest().estimateGas({
+        from: accounts,
+        value: web3.utils.toWei(value.toString(), "ether"),
+      });
       console.log("gas", gas.toString());
       const nonce = await web3.eth.getTransactionCount(accounts);
 
@@ -91,32 +123,30 @@ function App() {
         })
         .on("receipt", async (receipt) => {
           console.log(receipt);
-          console.log({   transactionHash:receipt.transactionHash,
-            investment:value,
-            address:accounts
+          console.log({
+            transactionHash: receipt.transactionHash,
+            investment: value,
+            address: accounts,
           });
-          await axios.post("http://localhost:3333/api/v1/saveTxData",{
-            transactionHash:receipt.transactionHash,
-            investment:value,
-            address:accounts
-          })
+          await axios.post("http://localhost:3333/api/v1/saveTxData", {
+            transactionHash: receipt.transactionHash,
+            investment: value,
+            address: accounts,
+          });
         });
     } catch (err) {
       console.log(err);
     }
   }
-
 
   async function withdrawAdmin() {
     try {
       let gasPrice = await web3.eth.getGasPrice();
       console.log("gasPrice", gasPrice);
 
-      let gas = await contract.methods
-        .withdraw()
-        .estimateGas({
-          from: accounts
-        });
+      let gas = await contract.methods.withdraw().estimateGas({
+        from: accounts,
+      });
       console.log("gas", gas.toString());
       const nonce = await web3.eth.getTransactionCount(accounts);
 
@@ -127,31 +157,56 @@ function App() {
           to: address,
           gas,
           gasPrice,
-          nonce
+          nonce,
         })
         .on("transactionHash", (tx) => {
           console.log(tx);
         })
         .on("receipt", async (receipt) => {
           console.log(receipt);
-       
         });
     } catch (err) {
       console.log(err);
     }
   }
+  async function withdrawUser() {
+    try {
+      let gasPrice = await web3.eth.getGasPrice();
+      console.log("gasPrice", gasPrice);
 
+      let gas = await contract.methods.userWithdraw().estimateGas({
+        from: accounts,
+      });
+      console.log("gas", gas.toString());
+      const nonce = await web3.eth.getTransactionCount(accounts);
 
+      await contract.methods
+        .userWithdraw()
+        .send({
+          from: accounts,
+          to: address,
+          gas,
+          gasPrice,
+          nonce,
+        })
+        .on("transactionHash", (tx) => {
+          console.log(tx);
+        })
+        .on("receipt", async (receipt) => {
+          console.log(receipt);
+        });
+    } catch (err) {
+      console.log(err);
+    }
+  }
   async function claimToken() {
     try {
       let gasPrice = await web3.eth.getGasPrice();
       console.log("gasPrice", gasPrice);
 
-      let gas = await contract.methods
-        .claimToken()
-        .estimateGas({
-          from: accounts
-        });
+      let gas = await contract.methods.claimToken().estimateGas({
+        from: accounts,
+      });
       console.log("gas", gas.toString());
       const nonce = await web3.eth.getTransactionCount(accounts);
 
@@ -162,30 +217,30 @@ function App() {
           to: address,
           gas,
           gasPrice,
-          nonce
+          nonce,
         })
         .on("transactionHash", (tx) => {
           console.log(tx);
         })
         .on("receipt", async (receipt) => {
           console.log(receipt);
-       
         });
     } catch (err) {
       console.log(err);
     }
   }
-
-
-
 
   return (
     <>
       <p>{accounts}</p>
       <h1>ICO</h1>
       <div>
+        <p>Start date:{startDate} </p>
+        <p>End date:{endDate} </p>
+        <p>Soft Cap:{softCap} ETH</p>
+        <p>Hard Cap:{hardCap} ETH</p>
         <p>Raise Amount:{raisedAmount} ETH</p>
-        <p>your contribution:{lockBalance} ETH</p>
+        <p>your Contribution:{lockBalance} ETH</p>
         <p>Token Balance:{tokenBalance} </p>
         <input
           type="number"
@@ -193,30 +248,51 @@ function App() {
           onChange={(e) => setValue(e.target.value)}
         />
       </div>
-      <div style={{display:"flex"}}>
-      <div className="card">
-        <button onClick={invest}>invest</button>
-      </div>
-      {isAdmin?(
-      <div className="card">
-        <button onClick={withdrawAdmin}>withdraw Admin</button>
-      </div>
-      ):null}
+      <div >
+        {Math.floor(Date.now() / 1000) >
+        (async () => Number(await contract?.methods?.closeTime().call()))() ? (
+          <div className="card">
+            <button onClick={invest}>invest</button>
+          </div>
+        ) : (
+          <></>
+        )}
+        {isAdmin ? (
+          <div className="card">
+            <button onClick={withdrawAdmin}>withdraw Admin</button>
+          </div>
+        ) : (
+          <></>
+        )}
 
-    {isAdmin?(
-      <div className="card">
-        <button onClick={claimToken}>claim Token</button>
-      </div>
-      ):null}
+        {Number(raisedAmount) >= Number(softCap) ? (
+          <div className="card">
+            <button onClick={claimToken}>claim Token</button>
+          </div>
+        ) : (
+          <></>
+        )}
+
+        {Number(raisedAmount) > Number(softCap) &&
+        Math.floor(Date.now() / 1000) >
+          (async () =>
+            Number(await contract?.methods?.closeTime().call()))() ? (
+          <div className="card">
+            <button onClick={withdrawUser}>withdraw User</button>
+          </div>
+        ) : (
+          <></>
+        )}
       </div>
       <ul>
-        {txData.map((i,index)=>(
-
-        <li key={index}>  
-          <a href={`https://sepolia.arbiscan.io/tx/${i.transactionHash}`}>Tx Hash</a>
-          <div>{i.address}</div> 
-          <div>{i.investment} ETH</div> 
-        </li>
+        {txData?.map((i, index) => (
+          <li key={index}>
+            <a href={`https://sepolia.arbiscan.io/tx/${i?.transactionHash}`}>
+              Tx Hash
+            </a>
+            <div>{i?.address}</div>
+            <div>{i?.investment} ETH</div>
+          </li>
         ))}
       </ul>
     </>
